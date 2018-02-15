5c0eea4e1b72e7ac4d7a9b8ef38743fe93e02034
Azzret <Jusuev.azret@bk.ru>
2018-02-15 17:17:26 +0300

    Create button

--- Urmat/Urmat/Form1.Designer.cs
+++ Urmat/Urmat/Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(42, 89);
+            this.button1.Location = new System.Drawing.Point(21, 41);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -44,17 +45,28 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(56, 47);
+            this.label1.Location = new System.Drawing.Point(180, 46);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Urmat I ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -67,7 +79,15 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+<<<<<<< HEAD
+<<<<<<< HEAD
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+=======
+        private System.Windows.Forms.Button button2;
+>>>>>>> e890e54d06c33c59a0fd13e9950598ef5eb4cfbd
+=======
+>>>>>>> 442694c08bbcb01716808849d70b721c0f474536
     }
 }
 
